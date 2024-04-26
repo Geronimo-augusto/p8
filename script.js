f164c0e159646fd71de3a53fd8202ca2128597e5
geronimo <eduardinhggsouza@gmail.com>
2024-04-26 09:27:00 -0300

    mete funda

---
+++ script.js
@@ -0,0 +1,7 @@
+let p1=prompt("manda a call ")
+let p2=prompt("digite sua gayzise")
+let p3=prompt("fala deu brother")
+let p4=prompt("qual quarto? ")
+let msm=document.getElementById("msm")
+
+msm.innerHTML=`hola ${p1}, seu coiso gay é ${p2} e vai fazer no ${p3} no(em) ${p4}`
